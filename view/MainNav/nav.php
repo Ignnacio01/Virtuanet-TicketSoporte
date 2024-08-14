a256ce858be1d92f243bb6fb212cbbf3d2fa57c1
Oscar <oscargonvargas15@gmail.com>
2024-08-14 12:57:36 -0400

    Segundo Commit

---
+++ view/MainNav/nav.php
@@ -0,0 +1,119 @@
+<?php
+    /* TODO: Rol 1 es de Usuario */
+    if ($_SESSION["rol_id"]==1){
+        ?>
+            <nav class="side-menu">
+                <ul class="side-menu-list">
+                    <li class="blue-dirty">
+                        <a href="..\Home\">
+                            <span class="glyphicon glyphicon-th"></span>
+                            <span class="lbl">Inicio</span>
+                        </a>
+                    </li>
+
+                    <li class="blue-dirty">
+                        <a href="..\NuevoTicket\">
+                            <span class="glyphicon glyphicon-th"></span>
+                            <span class="lbl">Nuevo Ticket</span>
+                        </a>
+                    </li>
+
+                    <li class="blue-dirty">
+                        <a href="..\ConsultarTicket\">
+                            <span class="glyphicon glyphicon-th"></span>
+                            <span class="lbl">Consultar Ticket</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        <?php
+    }else if ($_SESSION["rol_id"]==2){
+        ?>
+            <nav class="side-menu">
+                <ul class="side-menu-list">
+                    <li class="blue-dirty">
+                        <a href="..\Home\">
+                            <span class="glyphicon glyphicon-th"></span>
+                            <span class="lbl">Inicio</span>
+                        </a>
+                    </li>
+
+                    <li class="blue-dirty">
+                        <a href="..\NuevoTicket\">
+                            <span class="glyphicon glyphicon-th"></span>
+                            <span class="lbl">Nuevo Ticket</span>
+                        </a>
+                    </li>
+
+                    <li class="blue-dirty">
+                        <a href="..\MntPrioridad\">
+                            <span class="glyphicon glyphicon-th"></span>
+                            <span class="lbl">Editar Prioridades</span>
+                        </a>
+                    </li>
+
+                    <li class="blue-dirty">
+                        <a href="..\MntCategoria\">
+                            <span class="glyphicon glyphicon-th"></span>
+                            <span class="lbl">Editar Categorias</span>
+                        </a>
+                    </li>
+
+                    <li class="blue-dirty">
+                        <a href="..\ConsultarTicket\">
+                            <span class="glyphicon glyphicon-th"></span>
+                            <span class="lbl">Consultar Ticket</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        <?php
+    }else{
+        ?>
+        <nav class="side-menu">
+        <ul class="side-menu-list">
+            <li class="blue-dirty">
+                <a href="..\Home\">
+                    <span class="glyphicon glyphicon-th"></span>
+                    <span class="lbl">Inicio</span>
+                </a>
+            </li>
+
+            <li class="blue-dirty">
+                <a href="..\MntUsuario\">
+                    <span class="glyphicon glyphicon-th"></span>
+                    <span class="lbl">Editar Usuarios</span>
+                </a>
+            </li>
+
+            <li class="blue-dirty">
+                <a href="..\MntPrioridad\">
+                    <span class="glyphicon glyphicon-th"></span>
+                    <span class="lbl">Editar Prioridades</span>
+                </a>
+            </li>
+
+            <li class="blue-dirty">
+                <a href="..\MntCategoria\">
+                    <span class="glyphicon glyphicon-th"></span>
+                    <span class="lbl">Editar Categorias</span>
+                </a>
+            </li>
+
+            <li class="blue-dirty">
+                <a href="..\ConsultarTicket\">
+                    <span class="glyphicon glyphicon-th"></span>
+                    <span class="lbl">Consultar Ticket</span>
+                </a>
+            </li>
+
+            <li class="blue-dirty">
+                <a href="..\Reporte\">
+                <span class="glyphicon glyphicon-th"></span>
+                <span class="lbl">Generar Reporte</span>
+                </a>
+        </ul>
+    </nav>
+<?php
+}
+?>
